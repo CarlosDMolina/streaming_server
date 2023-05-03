7afc62f92cb65cc9895c18f021c34b5660f4951a
CarlosDMolina <cakimolina86@gmail.com>
2023-05-02 21:26:16 -0500

    final version

--- convert.py
+++ convert.py
@@ -18,7 +18,6 @@ def convert_video(video_path, dest_path):
         total_pixels = frame.shape[0] * frame.shape[1]
         depth = int(math.log(total_pixels, 4))
         size = 2 ** depth
-
     count = 0
 
     while True:
@@ -46,4 +45,44 @@ def convert_video(video_path, dest_path):
 
     # Release the video capture and close all windows
     cap.release()
+    cv2.destroyAllWindows()
+
+
+def resize_video(input_video_path, output_video_path, new_size):
+    # Open the video file
+    cap = cv2.VideoCapture(input_video_path)
+
+    # Get the video's original properties
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    # Calculate the new dimensions
+    new_width = new_size
+    new_height = new_size
+
+    # Initialize the VideoWriter
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_video_path, fourcc,
+                          fps, (new_width, new_height))
+
+    while True:
+        # Read a frame from the video
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        # Resize and crop the frame
+        ratio = new_size / height
+        frame = cv2.resize(frame, (int(width * ratio), int(height * ratio)))
+        delta = (frame.shape[1] - new_size) // 2
+        frame = frame[:, delta:delta+new_size]
+
+        # Write the resized frame to the output video
+        out.write(frame)
+
+    # Release resources
+    cap.release()
+    out.release()
     cv2.destroyAllWindows()
---
+++ real_time_stream.py
@@ -0,0 +1,68 @@
+import cv2
+from Quadtree import *
+from concurrent.futures import ProcessPoolExecutor
+
+
+def process_frames(frame, range):
+    qtree = Qtree()
+    qtree.insert(frame, range=range)
+    processed_frame = qtree.reconstruct_image()
+    return processed_frame
+
+
+def process_frame(args):
+    frame, range = args
+    return process_frames(frame, range)
+
+
+def on_change(x):
+    pass
+
+
+def main():
+    video = 'surf_square.mp4'
+    title = "Video Streaming Server"
+    trackbar = "Bandwidth"
+
+    cap = cv2.VideoCapture(video)
+
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    delay = int(1000 / fps)
+
+    frame_size = 512
+
+    cv2.namedWindow(title,
+                    cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
+    cv2.resizeWindow(title, frame_size, frame_size)
+
+    max_range = 100
+    cv2.createTrackbar(trackbar, title,
+                       100, max_range, on_change)
+
+    executor = ProcessPoolExecutor()
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            continue
+
+        range = 100 - cv2.getTrackbarPos(trackbar, title)
+
+        frame = cv2.resize(frame, (frame_size, frame_size))
+
+        future = executor.submit(process_frame, (frame, range))
+        qtree_frame = future.result()
+
+        cv2.imshow(title, qtree_frame)
+
+        key = cv2.waitKey(delay) & 0xFF
+        if key == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
